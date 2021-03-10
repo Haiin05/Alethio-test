d9e6c0673bc1796e755a2c848f5bbef42afa9e43
haiin <h986680@gmail.com>
2021-03-10 12:16:49 +0900

    [Add] MypageDetail layout, function, Mypage amended

---
+++ src/Pages/MyPage/Components/Pagination.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Pagination = ({ pageArr, setCurrentPage }) => {
+  const goToPage = (pageNum) => {
+    setCurrentPage(pageNum);
+  };
+  return (
+    <PaginationWrapper>
+      {pageArr?.map((num, idx) => {
+        return (
+          <Btn key={idx} onClick={() => goToPage(idx + 1)}>
+            {num + 1}
+          </Btn>
+        );
+      })}
+    </PaginationWrapper>
+  );
+};
+
+export default withRouter(Pagination);
+
+const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+`;
+
+const Btn = styled.button`
+  margin-right: 10px;
+  border: none;
+  font-size: 20px;
+  background-color: white;
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+`;
---
+++ src/Pages/MyPageDetail/MyPageDetail.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
+import Itembox from '../../Components/Itembox/Itembox';
+const MyPageDetail = ({ history }) => {
+  return (
+    <MyPageDetailWrapper>
+      <Itembox
+        id={history.location.state.id}
+        name={history.location.state.name}
+      />
+    </MyPageDetailWrapper>
+  );
+};
+
+export default withRouter(MyPageDetail);
+
+const MyPageDetailWrapper = styled.ul`
+  width: 100%;
+  max-width: 672px;
+  height: 100%;
+  margin: 0 auto;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
